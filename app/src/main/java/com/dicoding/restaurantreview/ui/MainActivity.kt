1c4e090ffbd2f37012b37a642d98357e4ddf51f7
avrians <avriansyahb10@gmail.com>
2024-03-23 12:10:04 +0700

    [av][Latihan LiveData dengan Single Event][finish]

--- app/src/main/java/com/dicoding/restaurantreview/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/restaurantreview/ui/MainActivity.kt
@@ -46,7 +46,13 @@ class MainActivity : AppCompatActivity() {
             showLoading(it)
         }
         mainViewModel.snackbarText.observe(this) {
-            Snackbar.make(window.decorView.rootView, it, Snackbar.LENGTH_SHORT).show()
+            it.getContentIfNotHandled()?.let { snackBarText ->
+                Snackbar.make(
+                    window.decorView.rootView,
+                    snackBarText,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+            }
         }
 
         binding.btnSend.setOnClickListener { view ->
